561ce2f9bf7daff7afb9189522067dd30c21d02b
Xavi Torelló <info@xaviertorello.cat>
2024-07-16 10:39:02 +0200

    Add "patrocina'ns" button
    with a mailto to our sponsors mailbox

--- app/routes/sponsor/_index.tsx
+++ app/routes/sponsor/_index.tsx
@@ -59,6 +59,29 @@ export default function SponsorPage() {
         </div>
       </div>
 
+      <a
+        href="mailto:patrocinis@geekscat.org"
+        target="_blank"
+        rel="noreferrer"
+        className="mt-6"
+      >
+        <button className="inline-flex items-center bg-neutral-900 text-white font-semibold text-lg px-8 py-4 rounded-full hover:bg-neutral-700 transition duration-200">
+          Patrocina'ns
+          <svg
+            className="ml-2 -mr-1 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </a>
+
       <h1 className="w-full font-bold text-3xl mt-10">
         Per <span className="pretty">250€</span> pots patrocinar la festa. Us
         <span className="pretty"> oferim:</span>
